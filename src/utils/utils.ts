a44a3ea1cb403bef1defab0bf22aa3dad03ed1db
ivanexDev <ivanexdev@gmail.com>
2023-12-22 18:35:43 -0300

    add doc to utils function

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,4 +1,6 @@
 /**
+ * Function that calculates the exchange from one base currency to a target currency. 
+ * If the value is less than 0 or NaN, it returns 0.
  * 
  * @param origin The amount in the original currency that you want to exchange for the second currency.
  * @param exchange 
